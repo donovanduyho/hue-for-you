3b9e05c809c5f67c8f443589956d45d0af20ac8a
dgarciaperales <dgarciaperales04@gmail.com>
2024-02-09 21:12:48 -0500

    Update script.js

--- site/script.js
+++ site/script.js
@@ -3,6 +3,14 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+
+const box = document.getElementById("input");
+box.addEventListener('input', function(event) {
+    const input = event.target.value;
+    console.log("input", input);    
+});
+
+
 function submitInput() {
     var input = document.getElementById("input").value;
     alert("Your entry was: " + input);

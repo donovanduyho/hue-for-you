c1eafe963e0813fee34ad5792328d0037bdc3962
dgarciaperales <dgarciaperales04@gmail.com>
2024-02-10 14:07:33 -0500

    Update app.js

--- front-end/app.js
+++ front-end/app.js
@@ -1,16 +1,6 @@
 import React from 'react';
 
 
-const express=require('express');
-const app=express();
-
-
-app.use(express.static('public'));
-
-const port =proces.env.PORT||3000;
-app.listen(port, ()=> {
-    console.log('server is running on another port');
-});
 // function script() {
 //     const [input, setInput] = useState('');
 

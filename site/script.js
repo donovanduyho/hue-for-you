5e0751c9f50198f4218c33b7828106ac84a444e7
dgarciaperales <dgarciaperales04@gmail.com>
2024-02-10 11:39:53 -0500

    Update script.js

--- site/script.js
+++ site/script.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import InfiniteScroll from 'react-infinite-scroller';
 
-function MyScroll() {
+// function MyScroll() {
 
-}
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("Yay the page has loaded!");
-});
+// }
 
 
 
-const box = document.getElementById("input");
-box.addEventListener('input', function(event) {
-    const input = event.target.value;
-    console.log("input", input);    
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//     console.log("Yay the page has loaded!");
+// });
 
 
-function submitInput() {
-    var input = document.getElementById("input").value;
-    console.log("Your entry was: " + input);
-    // alert("Your entry was: " + input);
 
-}
+// const box = document.getElementById("input");
+// box.addEventListener('input', function(event) {
+//     const input = event.target.value;
+//     console.log("input", input);    
+// });
+
+
+// function submitInput() {
+//     var input = document.getElementById("input").value;
+//     // console.log("Your entry was: " + input);
+//     alert("Your entry was: " + input);
+
+// }
 
 
 
@@ -42,32 +42,32 @@ function submitInput() {
 
 
 
-// function script() {
-//     const [input, setInput] = useState('');
+function script() {
+    const [input, setInput] = useState('');
 
 
-//     const handleInputChange = (event) => {
-//         setInput(event.target.value);
-//         console.log("input", event.target.value);
-//     };
+    const handleInputChange = (event) => {
+        setInput(event.target.value);
+        console.log("input", event.target.value);
+    };
 
-//     const handleSubmit = () => {
-//         alert("Youre entry was: " + input);
-//     };
-
-
-//     return (
-//         <div>
-//             <input
-//                 id="input"
-//                 type="text"
-//                 value = {input}
-//                 onChange = {handleInputChange}
-//             />
-//             <button onClick = {handleSubmit}>Submit</button>
-//         </div>
-//     );
-// }
+    const handleSubmit = () => {
+        alert("Youre entry was: " + input);
+    };
 
 
-// export default script;
+    return (
+        <div>
+            <input
+                id="input"
+                type="text"
+                value = {input}
+                onChange = {handleInputChange}
+            />
+            <button onClick = {handleSubmit}>Submit</button>
+        </div>
+    );
+}
+
+
+export default script;

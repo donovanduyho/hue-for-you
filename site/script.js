8191bc073932c6ee0ddf46e3419c5cf4f07eafbf
dgarciaperales <dgarciaperales04@gmail.com>
2024-02-10 10:51:18 -0500

    Update script.js

--- site/script.js
+++ site/script.js
@@ -1,50 +1,50 @@
-import React, {} from 'react';
+// import React, {} from 'react';
 
-function script() {
-    const [input, setInput] = useState('');
-
-
-    const handleInputChange = (event) => {
-        setInput(event.target.value);
-        console.log("input", event.target.value);
-    };
-
-    const handleSubmit = () => {
-        alert("Youre entry was: " + input);
-    };
+// function script() {
+//     const [input, setInput] = useState('');
 
 
-    return (
-        <div>
-            <input
-                id="input"
-                type="text"
-                value = {input}
-                onChange = {handleInputChange}
-            />
-            <button onClick = {handleSubmit}>Submit</button>
-        </div>
-    );
-}
+//     const handleInputChange = (event) => {
+//         setInput(event.target.value);
+//         console.log("input", event.target.value);
+//     };
+
+//     const handleSubmit = () => {
+//         alert("Youre entry was: " + input);
+//     };
 
 
-export default script;
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     console.log("Yay the page has loaded!");
-// });
-
-
-
-// const box = document.getElementById("input");
-// box.addEventListener('input', function(event) {
-//     const input = event.target.value;
-//     console.log("input", input);    
-// });
-
-
-// function submitInput() {
-//     var input = document.getElementById("input").value;
-//     alert("Your entry was: " + input);
+//     return (
+//         <div>
+//             <input
+//                 id="input"
+//                 type="text"
+//                 value = {input}
+//                 onChange = {handleInputChange}
+//             />
+//             <button onClick = {handleSubmit}>Submit</button>
+//         </div>
+//     );
 // }
+
+
+// export default script;
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Yay the page has loaded!");
+});
+
+
+
+const box = document.getElementById("input");
+box.addEventListener('input', function(event) {
+    const input = event.target.value;
+    console.log("input", input);    
+});
+
+
+function submitInput() {
+    var input = document.getElementById("input").value;
+    alert("Your entry was: " + input);
+}
